721ea25603ac7a7e6083130615b077f89dc01351
chino_desu <33673786+kafuuchino-desu@users.noreply.github.com>
2019-09-27 17:26:55 +0800

    color code for foreign members

--- Here.py
+++ Here.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 
 import re
+import time
 
 here_user = 0
+logfile = './plugins/here/'
+playerlist = []
+
+recordplayer = False
 
 def onServerStartup(server):
   global here_user
   here_user = 0
+  while recordplayer:
+    for player in playerlist:
+      #server.execute('data get entity ' + player)
+      time.sleep(5)
+
 
 def onServerInfo(server, info):
   global here_user
@@ -14,21 +24,45 @@ def onServerInfo(server, info):
   HIGHLIGHT_ENABLE = True
   dimension_convert = {"0":"主世界","-1":"地狱","1":"末地"}
   if (info.isPlayer == 0):
-    if("following entity data" in info.content and here_user>0):
-      name = info.content.split(" ")[0]
-      dimension = re.search("(?<=Dimension: )-?\d",info.content).group()
-      position_str = re.search("(?<=Pos: )\[.*?\]",info.content).group()
-      position = re.findall("\[(-?\d*).*?, (-?\d*).*?, (-?\d*).*?\]",position_str)[0]
-      position_show = "["+str(position[0])+","+str(position[1])+","+str(position[2])+"]"
-      if(WAYPOINT_SUPPORT):
-        pass
+    if("following entity data" in info.content):
+      if here_user>0:
+        name = info.content.split(" ")[0]
+        dimension = re.search("(?<=Dimension: )-?\d",info.content).group()
+        position_str = re.search("(?<=Pos: )\[.*?\]",info.content).group()
+        position = re.findall("\[(-?\d*).*?, (-?\d*).*?, (-?\d*).*?\]",position_str)[0]
+        position_show = "["+str(position[0])+","+str(position[1])+","+str(position[2])+"]"
+        if(WAYPOINT_SUPPORT):
+          pass
+        else:
+          if dimension == '0':
+            server.say("§e" + name + "§r @ §2" + dimension_convert[dimension] + position_show)
+          elif dimension == '1':
+            server.say("§e" + name + "§r @ §5" + dimension_convert[dimension] + position_show)
+          elif dimension == '-1':
+            server.say("§e" + name + "§r @ §4" + dimension_convert[dimension] + position_show)
+        if(HIGHLIGHT_ENABLE):
+          server.execute("effect give "+ name + " minecraft:glowing 15 1 true")
+          server.tell(name,"您将会被高亮15秒")
+        here_user-=1
       else:
-        server.say("§e" + name + "§r 在 §2" + dimension_convert[dimension] + position_show + "§r 向各位打招呼")
-      if(HIGHLIGHT_ENABLE):
-        server.execute("effect give "+ name + " minecraft:glowing 15 1 true")
-        server.tell(name,"您将会被高亮15秒")
-      here_user-=1
+        name = info.content.split(" ")[0]
+        dimension = re.search("(?<=Dimension: )-?\d",info.content).group()
+        position_str = re.search("(?<=Pos: )\[.*?\]",info.content).group()
+        position = re.findall("\[(-?\d*).*?, (-?\d*).*?, (-?\d*).*?\]",position_str)[0]
+        position_show = "["+str(position[0])+","+str(position[1])+","+str(position[2])+"]"
+        nowtime = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+        logname = time.strftime('%Y-%m-%d',time.localtime(time.time())) + '.log'
+        with open(logfile + logname,'a+') as handle:
+          handle.write(nowtime + ' : ' + name + ' is at ' + dimension_convert[dimension] + ' ' + position_show + '\n')
   else:
     if info.content.startswith('!!here'):
       here_user+=1
       server.execute("data get entity "+info.player)
+
+def onPlayerJoin(server, player):
+  global playerlist
+  playerlist.append(player)
+
+def onPlayerLeave(server, player):
+  global playerlist
+  playerlist.remove(player)
