533aa77492e0817e72ff23ac4f686fd867f5c23b
ra1ny-yuki <wyb99@hotmail.com>
2021-07-31 01:20:49 +0800

    bump version to 1.1.0-alpha1: Text format changed

--- Here.py
+++ Here.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
-import json
 import re
+import json
+
+from mcdreforged.api.rtext import *
+from mcdreforged.api.types import ServerInterface, Info
 
 PLUGIN_METADATA = {
 	'id': 'here',
-	'version': '1.0.0',
+	'version': '1.1.0-alpha1',
 	'name': 'Here',
 	'author': [
 		'Fallen_Breath',
@@ -17,20 +20,26 @@ PLUGIN_METADATA = {
 # 将其设为0以禁用高亮
 HIGHLIGHT_TIME = 15
 
+# set it to True to enable Xaero's Minimap waypoint
+# 将其设为True以显示Xaero的小地图/Voxelmap的路径点
+XAERO_WAYPOINT = True
+VOXEL_WAYPOINT = True
+
+
 here_user = 0
 
 
-def process_coordinate(text):
+def process_coordinate(text: str) -> tuple:
 	data = text[1:-1].replace('d', '').split(', ')
 	data = [(x + 'E0').split('E') for x in data]
 	return tuple([float(e[0]) * 10 ** int(e[1]) for e in data])
 
 
-def process_dimension(text):
+def process_dimension(text: str) -> str:
 	return text.replace(re.match(r'[\w ]+: ', text).group(), '', 1)
 
 
-def display(server, name, position, dimension):
+def display(server: ServerInterface, name: str, position: str, dimension: str):
 	x, y, z = position
 	dimension_convert = {
 		'minecraft:overworld': '0',
@@ -41,48 +50,67 @@ def display(server, name, position, dimension):
 		'"minecraft:the_end"': '1'
 	}
 	dimension_color = {
-		'0': '§2',
-		'-1': '§4',
-		'1': '§5'
+		'0': RColor.dark_green,
+		'-1': RColor.dark_red,
+		'1': RColor.dark_purple
+	}
+	dimension_coordinate_color = {
+		'0': RColor.green,
+		'-1': RColor.red,
+		'1': RColor.light_purple
 	}
 	dimension_display = {
-		'0': {'translate': 'createWorld.customize.preset.overworld'},
-		'-1': {'translate': 'advancements.nether.root.title'},
-		'1': {'translate': 'advancements.end.root.title'}
+		'0': 'createWorld.customize.preset.overworld',
+		'-1': 'advancements.nether.root.title',
+		'1': 'advancements.end.root.title'
 	}
 	if dimension in dimension_convert:  # convert from 1.16 format to pre 1.16 format
 		dimension = dimension_convert[dimension]
-	texts = [
-		'',
-		{
-			'text': '§e{}'.format(name),
-			'clickEvent':
-			{
-				'action': 'run_command',
-				'value': '/execute at {0} run tp {0}'.format(name)
-			}
-		},
-		'§r @ ',
-		dimension_color[dimension],  # hacky fix for voxelmap yeeting text color in translated text 
-		dimension_display[dimension],
-		' §b[x:{}, y:{}, z:{}]§r'.format(int(x), int(y), int(z))
-	]
+
+	# text base
+	texts = RTextList(
+		'§e{}§r'.format(name),
+		' @ ',
+		RTextTranslation(dimension_display[dimension], color=dimension_color[dimension]),
+		RText(' [{}, {}, {}]'.format(int(x), int(y), int(z)), dimension_coordinate_color[dimension])
+	)
+
+	# click event to add waypoint
+	if VOXEL_WAYPOINT:
+		texts.append( ' ',
+			RText('[+V]', RColor.aqua).h('§bVoxelmap§r: 点此以高亮坐标点, 或者Ctrl点击添加路径点').c(
+				RAction.run_command, '/newWaypoint [x:{}, y:{}, z:{}, dim:{}]'.format(
+					int(x), int(y), int(z), dimension
+				))
+			)
+	if XAERO_WAYPOINT:
+		texts.append( ' ',
+			RText('[+X]', RColor.gold).h('§6Xaeros Minimap§r: 点击添加路径点').c(
+				RAction.run_command, 'xaero_waypoint_add:{}:{}:{}:{}:{}:6:false:0:Internal_{}_waypoints'.format(
+					name + "'s Location", name[0], int(x), int(y), int(z), dimension.replace('minecraft:', '').strip()
+				)
+			)
+		)
+	
 	if dimension in ['0', '-1']:  # coordinate convertion between overworld and nether
 		dimension_opposite = '-1' if dimension == '0' else '0'
 		x, z = (x / 8, z / 8) if dimension == '0' else (x * 8, z * 8)
-		texts.extend([
+		texts.append(
 			' §7->§r ',
-			dimension_color[dimension_opposite],
-			dimension_display[dimension_opposite],
-			' ({}, {}, {})'.format(int(x), int(y), int(z))
-		])
-	server.execute('tellraw @a {}'.format(json.dumps(texts)))
+			RText('[{}, {}, {}]'.format(int(x), int(y), int(z)), dimension_coordinate_color[dimension_opposite]).h(
+				RTextTranslation(dimension_display[dimension_opposite], color=dimension_color[dimension_opposite])
+			)
+		)
+
+	server.say(texts)
+
+	# highlight
 	global HIGHLIGHT_TIME
 	if HIGHLIGHT_TIME > 0:
 		server.execute('effect give {} minecraft:glowing {} 0 true'.format(name, HIGHLIGHT_TIME))
 
 
-def on_info(server, info):
+def on_info(server: ServerInterface, info: Info):
 	global here_user
 	if info.is_player and info.content == '!!here':
 		if hasattr(server, 'MCDR') and server.is_rcon_running():
@@ -102,5 +130,5 @@ def on_info(server, info):
 		here_user -= 1
 
 
-def on_load(server, old):
+def on_load(server: ServerInterface, old):
 	server.register_help_message('!!here', '广播坐标并高亮玩家')
--- README.md
+++ README.md
@@ -7,6 +7,10 @@ Here
 
 调整代码中的 `HIGHLIGHT_TIME` 即可控制玩家是否高亮，以及自定义高亮的时长
 
+将代码中的`VOXEL_WAYPOINT`设为`True`以显示一小段含点击事件的文本，点击以高亮坐标，ctrl点击以添加坐标点至Voxelmap
+
+或`XAERO_WAYPOINT`设置为`True`以显示一小段含点击事件的文本，点击以添加坐标点至Xaero's Minimap
+
 当 MCDR 启动 rcon 时此插件可使用 rcon 来获得玩家信息，响应更快
 
-![example](https://raw.githubusercontent.com/TISUnion/Here/MCDR/img.png)
+![example](./img.png)
